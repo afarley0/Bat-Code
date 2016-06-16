b46e2a92f901ceba25ee2caff9e505653f8d78ea
afarley0 <afarley0@vt.edu>
2016-06-16 09:21:30 -0400

    Update arduinoInterface0_2.m
    help section added

--- arduinoInterface0_2.m
+++ arduinoInterface0_2.m
@@ -1,5 +1,17 @@
-%% This code is written in MatLab. This code is designed to communicate with the Arduino via serial communication
-%% This 
+%% This code is written in MatLab. This code is designed to communicate with the Arduino via serial communication. This program runs with motorControl6.2.ino uploaded to the arduino.
+%   The stepper Motor
+%   INPUTS:_______________________
+%   noseStep an integer of how steps you wish the nose to travel. A step is
+%   equal to 1.8 degrees of motion.
+%   EX: noseStep = 100;
+%   earStep an integer of how steps you wish the ear to travel. A step is
+%   equal to 1.8 degrees of motion.
+%   EX: earStep = 100;
+%   Commands:_____________________
+%   No input commands are nessecary after the initialization. In order to
+%   successfully trigger the dynamic control, a digital high voltage signal
+%   must be sent to pin 52 on the arduino.
+%   
 arduino=serial('COM1','BaudRate',9600);
 
 fopen(arduino);
